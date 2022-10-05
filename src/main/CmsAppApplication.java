ffe3e3112af48b3f55fdfd1fa86a5c49eb1e8b77
siddesh <siddusrb@gmail.com>
2022-10-05 22:49:46 +0530

    uploading main class

---
+++ src/main/CmsAppApplication.java
@@ -0,0 +1,12 @@
+package com.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmsAppApplication.class, args);
+	}
+}
